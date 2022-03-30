8a2c93d358bbe52e2d2f44fb470c2b5b3d336faf
cao đức tín <102477829+caoductin@users.noreply.github.com>
2022-03-30 10:04:35 +0700

    Update baitap.cpp

--- baitap.cpp
+++ baitap.cpp
@@ -1,164 +1,107 @@
-#include<stdio.h>
-#include<string.h>
-typedef struct{
-	int ngay,thang,nam;
-}date;
-void NhapNgaySinh(date &ngaysinh)
-{
-	scanf("%d%d%d",&ngaysinh.ngay,&ngaysinh.thang,&ngaysinh.nam);	
-}
 
-void outputDate(date date1)
-{
-	printf("\nNgay sinh: %d/%d/%d", date1.ngay, date1.thang, date1.nam);
+void gameover(){	
+    SetColor(4);
+    system("cls");
+	gotoxy(30,9);
+	printf("....Game over....");
+	gotoxy(15,11);
+	printf("Choi ngu duoc co %d diem, an phim bat ki de ve menu",score);
+	gotoxy(27,13);
+	printf("|---- anh's thanh ----|");
+	getchar();
+	state = MENU;	
 }
-
-typedef struct sinhvien{
-	char masv[10];
-	char hoten[100];
-	float diemTHDC,diemKTLT;
-	date ngaysinh;
-}S;
-struct giangvien{
-	char magv[10];
-	char hoten[100];
-	date ngaysinh;
-};
-struct monhoc{
-	char mamonhoc[10];
-	char tenmonhoc[15];
-	int TC;
-};
-void Nhap(struct sinhvien &a){
-	printf("Nhap ten sinh vien: ");
-	fflush(stdin);
-	gets(a.hoten);
-	printf("\nNhap ma sinh vien: ");
-	fflush(stdin);
-	scanf("%s", &a.masv);
-	printf("\nNhap ngay sinh: ");
-	NhapNgaySinh(a.ngaysinh);
-	printf("\nNhap diem TDHC: ");
-	scanf("%f",&a.diemTHDC);
-	printf("\nNhap diem KTLT: ");
-	scanf("%f",&a.diemKTLT);
-}
-void NhapDanhSach(struct sinhvien DS[], int n){
-	for(int i=0; i<n; i++){
-		printf("\nNhap sinh vien thu %d: \n", i+1);
-		Nhap(DS[i]);
-	}
-}
-void Xuat(struct sinhvien a){
-	printf("\tTen sinh vien: %s\n", a.hoten);
-	printf("\tMa so sinh vien: %s\n\t", a.masv);
-	outputDate(a.ngaysinh);
-	printf("\tDiem THDC va KTLT: %.2f  %.2f",a.diemTHDC,a.diemKTLT);
-}
-void XuatDanhSach(struct sinhvien DS[], int n){
-	printf("\n*****************************\n");
-	for(int i=0; i<n; i++){
-		printf("\nThong tin sinh vien thu %d:\n",i+1);
-		Xuat(DS[i]);
-	}
-}
-void Nhap2(struct giangvien &b){
-	printf("\nNhap ten giang vien: ");
-	fflush(stdin);
-	gets(b.hoten);
-	printf("\nNhap ma giang vien: ");
-	fflush(stdin);
-	scanf("%s", &b.magv);
-	printf("\nNhap ngay sinh: ");
-	NhapNgaySinh(b.ngaysinh);
-}
-void NhapDanhSach2(struct giangvien DS2[], int n){
-	for(int i=0; i<n; i++){
-		printf("Nhap giang vien thu %d: \n", i+1);
-		Nhap2(DS2[i]);
-	}
-}
-void Xuat2(struct giangvien b){
-	printf("\tTen giang vien: %s\n", b.hoten);
-	printf("\tMa so giang vien: %s\n", b.magv);
-	outputDate(b.ngaysinh);
-}
-void XuatDanhSach2(struct giangvien DS2[], int n){
-	printf("\n*****************************\n");
-	for(int i=0; i<n; i++){
-		printf("Thong tin giang vien thu %d:\n",i+1);
-		Xuat2(DS2[i]);
-	}
-}
-void Nhap3(struct monhoc &c){
-	printf("Nhap ten mon hoc: ");
-	fflush(stdin);
-	gets(c.mamonhoc);
-	printf("Nhap ma mon hoc: ");
-	fflush(stdin);
-	scanf("%s",&c.tenmonhoc);
-	printf("Nhap so tin chi: ");
-	scanf("%d",&c.TC);
-}
-void NhapDanhSach3(struct monhoc DS3[], int n){
-	for(int i=0; i<n; i++){
-		printf("Nhap mon hoc thu %d: \n", i+1);
-		Nhap3(DS3[i]);
-	}
-}
-void Xuat3(struct monhoc c){
-	printf("\tTen mon hoc: %s\n", c.tenmonhoc);
-	printf("\tMa mon hoc: %s\n", c.mamonhoc);
-	printf("\tSo tin chi: %d\n", c.TC);
-}
-void XuatDanhSach3(struct monhoc DS3[], int n){
-	printf("\n*****************************\n");
-	for(int i=0; i<n; i++){
-		printf("Thong mon hoc thu %d:\n",i+1);
-		Xuat3(DS3[i]);
-	}
-}
-void DoiCho(int &a,int &b){
-	int hoandoi=a;
-	a=b;
-	b=hoandoi;
-}
-// sap xep theo nam sinh
-void sx(sinhvien a[100],int n){
-	for(int i = 0;i < n - 1;i++){
-		for(int j = i+1;j < n;j++){
-			if(a[i].ngaysinh.nam > a[j].ngaysinh.nam)
-				DoiCho(a[i].ngaysinh.nam,a[j].ngaysinh.nam);
-			else if (a[i].ngaysinh.nam = a[j].ngaysinh.nam){
-				if(a[i].ngaysinh.thang > a[j].ngaysinh.thang)
-					DoiCho(a[i].ngaysinh.thang,a[j].ngaysinh.thang);
-				else if(a[i].ngaysinh.thang=a[i].ngaysinh.thang){
-					if(a[i].ngaysinh.ngay>a[j].ngaysinh.ngay){
-						DoiCho(a[i].ngaysinh.ngay,a[j].ngaysinh.ngay);
-					}
-				}
+void menu(){
+	SetColor(14);
+	system("cls");
+    gotoxy(25,3);
+	printf("Game ran an moi ");
+	gotoxy(30,5);
+	SetColor(4);
+	printf("1. vao game");
+	gotoxy(30,6);
+	printf("2. tuy chinh");
+	gotoxy(30,7);
+	printf("3. thong tin");
+	int choice = 1;
+	int oldChoice = 1;
+	int next = 1;
+	int key;
+	SetColor(14);
+	gotoxy(29,5);
+				printf("%c",16);
+	while(state==MENU){
+		if(kbhit()){
+			key = getch();
+			if(key==80) {
+			choice++;
+			if(choice ==4) choice = 1;
 			}
-	}
+			if(key==72) {
+			choice --;
+			if(choice == 0) choice = 3;
+			}
+		switch (choice) {
+			case 1 :
+				if(oldChoice!=choice){
+				
+				gotoxy(29,6);
+				printf(" ");
+				gotoxy(29,7);
+				printf(" ");
+				oldChoice = 1;
+				
+				gotoxy(29,5);
+				SetColor(14);
+				printf("%c",16);
+				}
+				break;
+			case 2 :
+				if(oldChoice!=choice){
+			
+				gotoxy(29,5);
+				printf(" ");
+				gotoxy(29,7);
+				printf(" ");
+		        gotoxy(29,6);
+		        SetColor(14);
+				printf("%c",16);
+				oldChoice = 2;
+				}
+				break;
+			case 3 :
+				if(oldChoice!=choice){
+				
+				
+				gotoxy(29,6);
+				printf(" ");
+				gotoxy(29,5);
+				printf(" ");
+				gotoxy(29,7);
+				SetColor(14);
+				printf("%c",16);
+				
+				oldChoice = 3;
+				}
+				break;
+		}
+		if(key==13){
+		switch (choice){
+		case 1:			
+			state = INGAME;
+			break;
+			case 2:
+				state = SETTING;
+				break;
+			case 3 :
+				state = INFORMATION;
+				break;
+				
+		}
+		}
 		
+	}
+	
+	}
+	
 }
-}
-int main(){
-	struct sinhvien a[100];
-	struct giangvien b[100];
-	struct monhoc c[50];
-	int d,e,f;
-	/*printf("Nhap so sinh vien: ");
-	scanf("%d",&d);
-	NhapDanhSach(a,d);
-    XuatDanhSach(a,d);
-    */
-    printf("\nNhap so giang vien: ");
-    scanf("%d",&e);
-    NhapDanhSach2(b,e);
-    XuatDanhSach2(b,e);
-	printf("Nhap so mon hoc: ");
-    scanf("%d",&f);
-    //NhapDanhSach3(c,f);
-	//XuatDanhSach3(c,f);
-}
-//caoductin
